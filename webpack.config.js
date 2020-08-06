24bb8e52a7c072cbb5da398b3206e82aa8224e2e
Cinzia Borello <cinziaborello@gmail.com>
2020-08-05 19:41:53 -0700

    Create structure for frontend and installed webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '/client/index.jsx'),
+  output: {
+    path: path.join(__dirname, '/public/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+
+};
+

d7b367ee0c39b35c25721dfaee727988947b57b4
Cinzia Borello <cinziaborello@gmail.com>
2020-08-09 11:53:58 -0700

    Refactor to get the photos data along with all the other data

--- server/index.js
+++ server/index.js
@@ -12,34 +12,36 @@ app.get('/', (req, res) => {
 });
 
 app.get('/zagat/restaurants/:id', (req, res) => {
-  const args1 = [req.params.id];
-  db.getTitle(args1, (err1, data1) => {
-    const restaurant = data1[0];
-    if (err1) {
-      res.status(404).send('error in getting the title info');
-    } else {
-      const args2 = [restaurant.category, restaurant.neighborhood, args1[0]];
-      db.getSimilar(args2, (err2, data2) => {
-        const similar = data2;
-        if (err2) {
-          res.status(404).send('error in getting similar restaurants');
-        } else {
-          res.status(200).send({ restaurant, similar });
-        }
+  const allData = {};
+  const argsTitle = [req.params.id];
+  return db.getTitle(argsTitle)
+    .then((dataTitle) => {
+      const restaurant = dataTitle[0];
+      allData.restaurant = {
+        name: restaurant.name,
+        category: restaurant.category,
+        neighborhood: restaurant.neighborhood,
+      };
+      const argsSimilar = [restaurant.category, restaurant.neighborhood, argsTitle[0]];
+      return db.getSimilar(argsSimilar);
+    })
+    .then((dataSimilar) => {
+      allData.similar = dataSimilar;
+      const results = dataSimilar.map((item) => {
+        const argsPhotos = [item.rid];
+        return Promise.resolve(db.getPhotos(argsPhotos));
       });
-    }
-  });
-});
-
-app.get('/zagat/photos/:id', (req, res) => {
-  const args = [req.params.id];
-  db.getPhotos(args, (err, data) => {
-    if (err) {
-      res.status(404).send('error in getting their photos');
-    } else {
-      res.status(200).send(data);
-    }
-  });
+      return Promise.all(results);
+    })
+    .then((dataPhotos) => {
+      allData.photos = dataPhotos;
+    })
+    .then(() => {
+      res.status(200).send(allData);
+    })
+    .catch(() => {
+      res.status(404).send('error in getting the restaurants data');
+    });
 });
 
 app.listen(port, () => {

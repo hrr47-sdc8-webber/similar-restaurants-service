32225e02ca63cec2f070ad34c28498376c05b239
Cinzia Borello <cinziaborello@gmail.com>
2020-08-12 20:24:44 -0700

    Add basic tests for Grid component

--- __tests__/Grid.test.js
+++ __tests__/Grid.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import { shallow, configure } from 'enzyme';
+import { shallow, mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Grid from '../client/components/Grid';
@@ -11,5 +11,11 @@ configure({ adapter: new Adapter() });
 describe('Grid', () => {
   it('renders without crashing', () => {
     shallow(<Grid />);
+  });
+
+  it('Grid renders snapshot correctly"', () => {
+    const component = renderer.create(<Grid />);
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
   });
 });

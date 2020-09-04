9c7e181f3ebc0fa0ca38769014ec4e04fab0ed80
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-09-04 14:34:07 -0700

    Edit artillery file and number generator to favor ids in last 10% of records

--- randomnumber.js
+++ randomnumber.js
@@ -1,6 +1,10 @@
 const generateRandomNumber = (userContext, events, done) => {
-  const num = Math.floor(Math.random() * 10000000) + 1;
-  userContext.vars.id = num;
+  const weight = Math.floor(Math.random() * 10) + 1;
+  if (weight <= 8) {
+    userContext.vars.id = Math.floor(Math.random() * 1000000) + 9000000;
+  } else {
+    userContext.vars.id = Math.floor(Math.random() * 10000000) + 1;
+  }
   return done();
 };
 
